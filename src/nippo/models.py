1b48a599e77d9fee0dfd7cdeadd0191e5d221ff0
yohoo49123 <yohoo49123@yahoo.co.jp>
2024-12-26 22:29:37 +0900

    20241226gitpullpushのテスト家

--- src/nippo/models.py
+++ src/nippo/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
+#git pull pushのテスト
+
 class NippoModel(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
     timestamp = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.title
