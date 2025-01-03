62979becc79a5c894a5777c63effaa405715ccef
yohoo49123 <yohoo49123@yahoo.co.jp>
2025-01-03 16:58:29 +0900

    20250103formpyにcssを適用動画23

--- src/nippo/forms.py
+++ src/nippo/forms.py
@@ -3,3 +3,9 @@ from django import forms
 class NippoFormClass(forms.Form):
     title = forms.CharField(label="タイトル",widget=forms.TextInput(attrs={'placeholder':'タイトル...'}))
     content = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'内容...'}),label="内容")
+
+
+    def __init__(self, *args, **kwargs):
+        for field in self.base_fields.values():
+            field.widget.attrs.update({"class":"form-control"})
+        super().__init__(*args, **kwargs)
