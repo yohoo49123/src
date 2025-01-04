6b27fee2b05a44541a21ae9f83757f697018c662
yohoo49123 <yohoo49123@yahoo.co.jp>
2025-01-05 00:13:27 +0900

    20250104viewの大まかな完成動画27

--- src/nippo/forms.py
+++ src/nippo/forms.py
@@ -1,11 +1,30 @@
 from django import forms
+from .models import NippoModel
 
-class NippoFormClass(forms.Form):
-    title = forms.CharField(label="タイトル",widget=forms.TextInput(attrs={'placeholder':'タイトル...'}))
-    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'内容...'}),label="内容")
+class NippoModelForm(forms.ModelForm):
+    class Meta:
+        model = NippoModel
+        fields = "__all__"
 
+    #フィールド全て
+    #fields = "__all__"
+
+    #フィールドの一部
+    #fields = ["フィールド1", "フィールド2"]
+
+    #一部のフィールドを除く
+    #exclude = "除きたいフィールド"
 
     def __init__(self, *args, **kwargs):
         for field in self.base_fields.values():
-            field.widget.attrs.update({"class":"form-control"})
+            field.widget.attrs["class"] = "form-control"
+        super().__init__(*args, **kwargs)
+
+class NippoFormClass(forms.Form):
+    title = forms.CharField(max_length=100, label="タイトル")
+    content = forms.CharField(widget=forms.Textarea(), max_length=1000, label="内容")
+
+    def __init__(self, *args, **kwargs):
+        for field in self.base_fields.values():
+            field.widget.attrs["class"] = "form-control"
         super().__init__(*args, **kwargs)
--- src/nippo/models.py
+++ src/nippo/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
 
-#git pull pushのテスト
-#git pull pushのテスト会社から
-
 class NippoModel(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.CharField(max_length=1000)
+    title = models.CharField(max_length=100, verbose_name="タイトル")
+    content = models.TextField(max_length=1000, verbose_name="内容")
     timestamp = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
