d90b11ba16e28af01e883049f94644f66ba3444f
yohoo49123 <yohoo49123@yahoo.co.jp>
2025-01-06 23:17:56 +0900

    20250106管理画面adminのカスタマイズ動画29

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+class UserAdmin(BaseUserAdmin):
+    #一覧表示
+    list_display = (
+        "email",
+        "active",
+        "staff",
+        "admin",
+    )
+    list_filter = (
+        "admin",
+        "active",
+    )
+    ordering = ("email",)
+    filter_horizontal = ()
+    search_fields = ('email',)
+
+    #新規登録用
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+
+    #編集用
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('権限', {'fields': ('staff','admin',)}),
+    )
+    
+admin.site.register(User, UserAdmin)
